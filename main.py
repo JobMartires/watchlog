3cb4d9be2751b5871222819f3de16cdb53b75202
Job Martires <jobmartires@gmail.com>
2025-09-04 22:45:44 +0800

    added readEntry func

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from datetime import datetime
+import csv
 import random
 
 def main():
-    choice = input(f"[1] Add [2] Read [3] Random [4] Exit")
+
+    choice = input(f"\n\n[1] Add [2] Read [3] Random [4] Exit\n")
 
     if choice == "1":
-        list = addEntry()
+        addEntry()
 
     elif choice == "2":
-        #display watchlog here
+        readEntry()
     
     elif choice == "3":
-        #random here
+        randomEntry()
 
     elif choice == "4":
-        #exit here
+        print(f"exit")
+
 
 def addEntry():
     month = datetime.today().month
@@ -27,8 +30,22 @@ def addEntry():
 
     entry = [title,season,episode,month,day,year]
     
-    with open("watchlog.csv", "w")  as f:
+    with open("watchlog.csv", "a")  as f:
         f.write(f"{entry}\n")
 
+    readEntry()
+
+def readEntry():
+    entries = []
+    with open("watchlog.csv", newline='') as f:
+        read = csv.reader(f)
+        for row in read:    
+            entries.append(", ".join(row))
+    for entry in entries:
+        row = entry.split(",")
+        
+
+def randomEntry():
+    print(f"Random Entry display here")
 
 main()
