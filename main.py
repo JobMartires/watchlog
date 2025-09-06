cfd1c3bc97356e28c0869a785664919318547673
JobM <jobmartires@gmail.com>
2025-09-06 22:41:43 +0800

    added remove entry option

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import csv
 import random
 
 def main():
-    choice = input(f"\n\n[1] Add [2] Read [3] Random\n")
+    choice = input(f"\n\n[1] Add [2] Read [3] Random [4] Remove\n")
     if choice == "1":
         addEntry()
     elif choice == "2":
         readEntry()
     elif choice == "3":
         randomEntry()
+    elif choice == "4":
+        removeEntry()
 
 # adds entry to the csv file
 def addEntry():
@@ -33,6 +35,9 @@ def readEntry():
             print(f"  {item[0].ljust(45)}   Se{item[1]}   Ep{item[2]}")
             print(f"════════════════════════════════════════════════════════════════")
 
+
+def removeEntry():
+    print(f"Remove entry function here")
 
 def randomEntry():
     print(f"Random Entry display here")
