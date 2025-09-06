7d78c6bcc7245721e461537bfcb79c7beefd504c
chibs <jobmartires@gmail.com>
2025-09-06 22:25:10 +0800

    readEntry function

--- main.py
+++ main.py
@@ -3,47 +3,36 @@ import csv
 import random
 
 def main():
-
-    choice = input(f"\n\n[1] Add [2] Read [3] Random [4] Exit\n")
-
+    choice = input(f"\n\n[1] Add [2] Read [3] Random\n")
     if choice == "1":
         addEntry()
-
     elif choice == "2":
         readEntry()
-    
     elif choice == "3":
         randomEntry()
 
-    elif choice == "4":
-        print(f"exit")
-
-
+# adds entry to the csv file
 def addEntry():
     month = datetime.today().month
     day = datetime.today().day
     year = datetime.today().year
-    
     title = input(f"Title: ")
     season = input(f"Season: ")
     episode = input(f"Episode: ")
-
-    entry = [title,season,episode,month,day,year]
-    
+    entry = (f"{title},{season},{episode},{month},{day},{year}")
     with open("watchlog.csv", "a")  as f:
-        f.write(f"{entry}\n")
-
+        f.write(f"{entry.replace("'","")}\n")
     readEntry()
 
+# access the csv and display each entry per row
 def readEntry():
-    entries = []
     with open("watchlog.csv", newline='') as f:
         read = csv.reader(f)
-        for row in read:    
-            entries.append(", ".join(row))
-    for entry in entries:
-        row = entry.split(",")
-        
+        for item in read:
+            print(f"════════════════════════════════════════════════════════════════")
+            print(f"  {item[0].ljust(45)}   Se{item[1]}   Ep{item[2]}")
+            print(f"════════════════════════════════════════════════════════════════")
+
 
 def randomEntry():
     print(f"Random Entry display here")
